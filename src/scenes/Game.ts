87a7eac877b9debd69b292b4d08065ee4a6edad6
JarettRoman <afrocious@gmail.com>
2024-02-09 13:08:49 -0500

    turn queue and linter

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,28 +1,77 @@
-import { Scene } from 'phaser';
+import { Scene } from 'phaser'
 
-export class Game extends Scene
-{
-    constructor ()
-    {
-        super('Game');
-    }
+class TurnQueue {
+  queue: Array<any>
+  place: number
+  constructor (players: Array<any>) {
+    this.queue = players.sort((a, b) => {
+      return b.dex - a.dex
+    })
+    this.place = 0
+  }
 
-    create ()
-    {
-        this.cameras.main.setBackgroundColor(0x00ff00);
+  getCurrentPlayer () {
+    return this.queue[this.place]
+  }
 
-        this.add.image(512, 384, 'background').setAlpha(0.5);
+  setNextPlayer () {
+    console.log('shifting to next player in queue')
+    this.place = (this.place + 1) % this.queue.length
+  }
+}
 
-        this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+export class Game extends Scene {
+  constructor () {
+    super('Game')
+  }
 
-        this.input.once('pointerdown', () => {
+  create () {
+    const playerList = [
+      {
+        name: 'Bob',
+        dex: 10
+      },
+      {
+        name: 'Shirley',
+        dex: 12
+      },
+      {
+        name: 'Quartz',
+        dex: 14
+      },
+      {
+        name: 'Alonzo',
+        dex: 8
+      }
+    ]
 
-            this.scene.start('GameOver');
+    const turnQueue = new TurnQueue(playerList)
 
-        });
-    }
+    console.log(turnQueue)
+
+    this.cameras.main.setBackgroundColor(0x00ff00)
+
+    this.add.image(512, 384, 'background').setAlpha(0.5)
+
+    this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
+      fontFamily: 'Arial Black',
+      fontSize: 38,
+      color: '#ffffff',
+      stroke: '#000000',
+      strokeThickness: 8,
+      align: 'center'
+    }).setOrigin(0.5)
+
+    this.input.keyboard?.on('keydown-A', () => {
+      console.log(turnQueue.getCurrentPlayer())
+    })
+
+    this.input.keyboard?.on('keydown-S', () => {
+      turnQueue.setNextPlayer()
+    })
+
+    this.input.once('pointerdown', () => {
+      this.scene.start('GameOver')
+    })
+  }
 }
